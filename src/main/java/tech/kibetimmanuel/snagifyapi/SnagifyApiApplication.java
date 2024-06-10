388de8400bcda34442a5586ad5fd07a4517c2f09
Kibeh <kibetimmanuel0@gmail.com>
2024-06-10 16:43:32 +0300

    initial commit:  create the project boilerplate with necessary dependencies

---
+++ src/main/java/tech/kibetimmanuel/snagifyapi/SnagifyApiApplication.java
@@ -0,0 +1,13 @@
+package tech.kibetimmanuel.snagifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnagifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnagifyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/kibetimmanuel/snagifyapi/SnagifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kibetimmanuel.snagifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnagifyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
